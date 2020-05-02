6a01a81a0759c1eab75cfcdd5f1d52ad8fe6f2ec
Prashant Singh <pacificlion123@gmail.com>
2020-05-02 08:02:34 +0530

    Added method to convert 2d array to list of list

--- src/main/java/com/pacificlion/leetcode/utilities/CollectionUtils.java
+++ src/main/java/com/pacificlion/leetcode/utilities/CollectionUtils.java
@@ -1,5 +1,6 @@
 package com.pacificlion.leetcode.utilities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,4 +14,16 @@ public class CollectionUtils {
 	public static <T> boolean compareEqual(List<T> A, List<T> B) {
 		  return A.containsAll(B)&& B.containsAll(A);
 	  }
+	
+	public static <T> List<List<T>> convert2DArrayToListOfList(T[][] array){
+		List<List<T>> list = new ArrayList<>();
+		for(T[] arr: array) {
+			List<T> tempList = new ArrayList<>();
+			for(T t: arr) {
+				tempList.add(t);
+			}
+			list.add(tempList);
+		}
+		return list;
+	}
 }
